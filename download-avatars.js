a8d2159d2ebe0c7f811a42d17d8094361d63f8e2
Chad Yehia <lex.yehia@icloud.com>
2017-10-09 14:48:46 +0000

    Step 4 done

--- download-avatars.js
+++ download-avatars.js
@@ -1,9 +1,15 @@
 const request = require('request')
 
+const GITHUB_USER  = "lexyehia",
+      GITHUB_TOKEN = "7201ee1afb501d72a00d043eee85d9910020d441"
+
+
 console.log("Welcome to the GitHub avatar downloader!")
 
-function getRepoContributors(repOwner, repName, cb) {
+function getRepoContributors(repoOwner, repoName, cb) {
+    const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`
 
+    console.log(requestURL)
 }
 
 getRepoContributors("jquery", "jquery", (err, result) => {
