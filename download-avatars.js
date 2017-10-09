e299628448e7dd20cb5011ec1c44b21a02db7d49
Chad Yehia <lex.yehia@icloud.com>
2017-10-09 15:56:39 +0000

    Finished project

--- download-avatars.js
+++ download-avatars.js
@@ -7,11 +7,20 @@ const GITHUB_USER  = "lexyehia",
 
 console.log("Welcome to the GitHub avatar downloader!")
 
+// Send a request for a list of avatar urls
 function getRepoContributors(repoOwner, repoName, cb) {
+
+    // Cancel execution and send message to console that arguments were not provided
+    if (!repoOwner) {
+        return console.log("Did not provide proper Repo Owner name as first argument")
+    } else if (!repoName) {
+        return console.log("Did not provide proper Rep Name as second argument")
+    }
+
     const options = {
         url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
         headers: {
-            'User-Agent': 'request'
+            'User-Agent': 'LHL Exercise'
         }
     }
 
@@ -21,14 +30,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
     })
 }
 
-function retrieveAvatars(response) {
-    response.forEach(r => {
-        downloadImageByURL(r['avatar_url'], `./avatars/${r['login']}.png`)
-    })
-}
-
+// For each contributor, pipe their avatar image to local file
 function downloadImageByURL(url, filePath) {
     request.get(url).pipe(fs.createWriteStream(filePath))
 }
 
-getRepoContributors("jquery", "jquery", retrieveAvatars)
+// Call the main function getRepoContributors and
+// provide anonymous callback that sets out where to store image files
+getRepoContributors(process.argv[2], process.argv[3], (response) => {
+    response.forEach(r => {
+        downloadImageByURL(r['avatar_url'], `./avatars/${r['login']}.png`)
+    })
+})
